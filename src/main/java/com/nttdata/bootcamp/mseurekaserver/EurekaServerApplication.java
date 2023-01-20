9f29d0978dede603ed326e31ccaa8b06152ef14f
evillarv <esthervillarv@gmail.com>
2023-01-19 22:23:59 -0500

    configuration EnableEurekaServer

--- src/main/java/com/nttdata/bootcamp/mseurekaserver/EurekaServerApplication.java
+++ src/main/java/com/nttdata/bootcamp/mseurekaserver/EurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.nttdata.bootcamp.mseurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class MsEurekaserverApplication {
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsEurekaserverApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
